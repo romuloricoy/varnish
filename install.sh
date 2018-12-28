ef697d0674076f10ae0a11c595fb8518f6abc4e1
Rômulo Ricoy <romuloricoy@gmail.com>
2018-12-28 16:16:29 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 apt-get -y update
-apt-get -y install varnish
+apt-get install -y debian-archive-keyring
+apt-get install -y curl gnupg apt-transport-https vim
+apt-get -y update
+curl -L https://packagecloud.io/varnishcache/varnish41/gpgkey | apt-key add -
+echo "deb https://packagecloud.io/varnishcache/varnish41/ubuntu/ xenial main"
+echo "deb-src https://packagecloud.io/varnishcache/varnish41/ubuntu/ xenial main"
+apt-get -y update
+apt-get install -y varnish
 
 tee /etc/varnish/default.vcl <<EOF
 vcl 4.0;
